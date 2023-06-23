1d6b822e0b24499df83ebd992f3d3304d8252ce3
VictorAtoms <99357424+VictorAtoms@users.noreply.github.com>
2023-06-23 14:07:03 -0600

    working server

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const bodyParser = require('body-parser');;
+const Shopify = require('shopify-api-node');
+
+const shopify = new Shopify({
+  shopName: 'atoms-apps-development',
+  apiKey: '50b5e45a359681bd2f85c820c1adb7dc',
+  password: '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'
+});
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/create-shipping-profile', async (req, res) => {
+  console.log(req)
+  const variables = await req.body.profile
+  const query = `mutation deliveryProfileCreate($profile: DeliveryProfileInput!) {
+    deliveryProfileCreate(profile: $profile) {
+      profile {
+        zoneCountryCount
+        name
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }`;
+  /* const variables = {
+    "profile": {
+      "name": "admin created",
+      "locationGroupsToCreate": [
+        {
+          "locations": [
+            "gid://shopify/Location/76352618802"
+          ],
+          "zonesToCreate": [
+            {
+              "countries": [
+                {
+                  "code": "MX",
+                  "includeAllProvinces": true
+
+                }
+              ],
+              "methodDefinitionsToCreate": [
+                {
+                  "active": true,
+                  "description": "",
+                  "name": "Creado desde admin",
+                  "rateDefinition": {
+                    "price": {
+                      "amount": "999",
+                      "currencyCode": "MXN"
+                    }
+                  }
+                }
+              ],
+              "name": "admin"
+            }
+          ]
+        }
+      ]
+    }
+  } */
+  shopify
+  .graphql(query, variables)
+  .then((customers) => console.log(customers))
+  .catch((err) => console.error(err));
+
+  /* try {
+    const configuration = new Configuration({
+      apiKey: process.env.DALLE_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log(response.data)
+    res.send(response.data);
+  } catch (error) {
+    console.log(error.data);
+    res.status(500).send('Error generating image');
+  } */
+});
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
