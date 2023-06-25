6daff78272200a588fd702e7791bb05c5f41c408
VictorAtoms <99357424+VictorAtoms@users.noreply.github.com>
2023-06-25 02:24:25 -0600

    await

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/create-shipping-profile', cors(), async (req, res) => {
   console.log(req.body)
   console.log(req.body.profile.locationGroupsToCreate)
   try{
-    const variables = req.body.profile
+    const variables = await req.body.profile
     const query = `mutation deliveryProfileCreate($profile: DeliveryProfileInput!) {
       deliveryProfileCreate(profile: $profile) {
         profile {
@@ -37,7 +37,7 @@ app.post('/create-shipping-profile', cors(), async (req, res) => {
         }
       }
     }`;
-    shopify
+    await shopify
     .graphql(query, variables)
     .then((profile) => {
       if(profile){
