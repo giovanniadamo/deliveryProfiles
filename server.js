813eca9a75e36d75a30685d89c1619df70d4724b
VictorAtoms <99357424+VictorAtoms@users.noreply.github.com>
2023-07-13 17:13:01 -0600

    env vars

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ app.post('/create-shipping-method', async (req, res) => {
 
 
 const server = app.listen(port, '127.0.0.1', () => {
-  const { address, port2 } = server.address();
-  console.log(`Server listening on port ${port2} ${address}`);
+  const { address } = server.address();
+  console.log(`Server listening on port ${port} ${address}`);
 });
 
